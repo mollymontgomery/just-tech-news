b107d360a22d95b7ba34a06549c008aad42f296d
Molly Montgomery <Molly@mollys-air-2.lan>
2022-01-04 19:45:44 -0600

    13.4

--- models/index.js
+++ models/index.js
@@ -1,14 +1,30 @@
 const User = require('./User');
 const Post = require("./Post");
+const Vote = require('./Vote');
 
-// create associations
-User.hasMany(Post, {
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
     foreignKey: 'user_id'
 });
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
 
